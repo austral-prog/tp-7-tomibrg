bee844f155af60afb5cc49ef33bb53b5d8a5c37b
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-05-21 13:18:38 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,20 @@
 def enumerate_list(list):
-    return list
+        new_list = []
+    for j in list:
+        if j:
+            new_list.append(j)
+    for i, j in enumerate(list):
+         new_list.append(f'{i}. {j}')    
+
+    return new_list
 
 
 def enumerate_backwards(list):
-    return list
+        reverse_list = []
+    for i in list:
+        if i:
+            reverse_list.append(i)
+    for i, j in enumerate(reverse_list):
+        reverse_list.append(f'{i}. {j[::-1]}')
+        
+    return reverse_list
