0f20accc563598b3e344dd06b1fa7a62485e7074
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-05-21 13:21:21 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,44 @@
 def index_of_by_index(word, list, index):
-    return -1
+    if word in list[index:]:
+        for i, j in enumerate(list[index:]):
+            if j == word:
+                return i + index
+    else:
+        return -1
 
 
 def index_of_empty(list):
-    return -1
+    if ''  in list:
+        for i, j in enumerate(list):
+            if j.strip() == '':
+                return i
+                break
+    else:
+        return -1
 
 
 def index_of(word, list):
-    return -1
+    if word in list:
+        for i, j in enumerate(list):  
+             if j == word:
+                return i
+                break
+    else:
+        return -1
 
 
 def put(word, list):
-    return -1
+    if index_of_empty(list) != -1:
+        list[index_of_empty(list)] = word
+        return list.index(word)
+    else:
+        return -1
 
 
 def remove(word, list):
-    return -1
+    counter = 0
+    for i, j in enumerate(list):
+        if j == word:
+            list[i] = ''
+            counter += 1
+    return counter
